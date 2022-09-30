b67c0f0b2457a25efdbabccf7f9c1cd64f42cb6a
DarkWolf3891 <jaimevarelarincon@outlook.es>
2022-09-30 16:31:37 -0500

    se incluye funcion para limpiar las localidades con uso de un diccionario

--- src/eda_limpieza.py
+++ src/eda_limpieza.py
@@ -1,7 +1,11 @@
 # Pseudo Codigo
 # 1. leer archivo .csv
-# 2. extraer el resumen
-# 3. guardar el resumen en formato .csv
+# 2. Eliminar registros duplicados
+# 3. Reemplazo la columna UNIDAD que tenia valores nulos por una columna nueva sin nulos
+# 4. Convierte str en formato de fecha
+# 5. Manipulacion Fechas (datetime)
+# 6. Consistencia de los Datos: Edad cambia el str por int
+# 7. Consistencia de los Datos: Localidad organiza los nombre usando un diccionario
 
 # importar librerias del sistema
 import numpy as np
@@ -83,11 +87,21 @@ def data_consistencia_edad(data):
     return data
 
 # Consistencia de los Datos: Localidad
-#def data_consistencia_localidad(data):
+def data_consistencia_localidad(data):
 
+    Localidades = { 1:'USAQUEN', 2:'CHAPINERO', 3:'SANTA FE', 4:'SAN CRISTOBAL', 5:'USME', 
+                    6:'TUNJUELITO', 7:'BOSA', 8: 'KENNEDY', 9: 'FONTIBON', 10: 'ENGATIVA',
+                    11: 'SUBA', 12: 'BARRIOS UNIDOS', 13: 'TEUSAQUILLO', 14: 'LOS MARTIRES',
+                    15: 'ANTONIO NARIÑO', 16: 'PUENTE ARANDA', 17: 'LA CANDELARIA', 18: 'RAFAEL URIBE URIBE',
+                    19: 'CIUDAD BOLIVAR', 20: 'SUMAPAZ'}
+
+    data['LOCALIDAD'] = data['CODIGO_LOCALIDAD'].map(Localidades)
+
+    return data 
+
+# Guarda el data frame limpio
 def save_data(data, filename):
-    # Guardar la tabla:
-    
+        
     data_dir = "processed"
     out_name = 'Clean_' + filename
     out_path = os.path.join(root_dir, "data", data_dir, out_name)
@@ -101,7 +115,7 @@ def main():
     data = data_sin_dato(data)
     data = data_date_time(data)
     data = data_consistencia_edad(data)
-    
+    data = data_consistencia_localidad(data)
     save_data(data, filename)
 
     print('DONE!!!')
